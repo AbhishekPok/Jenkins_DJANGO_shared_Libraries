d57a185bb31ae6f8cda4d74a7b7edd1a3c4bf101
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-12-16 13:25:25 +0545

    Fix Docker image tag and push commands

--- vars/pushtheimagetodockerhub.groovy
+++ vars/pushtheimagetodockerhub.groovy
@@ -3,7 +3,7 @@ def call(String projectname, String imagename, String dockerhubuser) {
         withCredentials([usernamePassword('credentialsId':"Docker Hub Cred", passwordVariable:"dockerHubPass", usernameVariable:"dockerhubUser")])
         {
             sh "docker login -u ${env.dockerhubUser} -p ${env.dockerHubPass}"
-            sh "docker image tag "${projectname}":"${imagename}" ${env.dockerhubUser}/"${projectname}":"${imagename}""
-            sh "docker push ${env.dockerhubUser}/"${projectname}":"${imagename}""
+            sh "docker image tag ${projectname}":${imagename} ${env.dockerhubUser}/${projectname}:${imagename}"
+            sh "docker push ${env.dockerhubUser}/${projectname}:${imagename}"
         }
 }
