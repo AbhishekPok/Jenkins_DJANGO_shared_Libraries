d5a968f12e8b3ff7a322398f6cecfdb74477e195
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-12-16 13:20:05 +0545

    Refactor Docker push function to accept parameters

--- vars/pushtheimagetodockerhub.groovy
+++ vars/pushtheimagetodockerhub.groovy
@@ -1,9 +1,9 @@
-def call() {
+def call(String projectname, String imagename, String dockerhubuser) {
   echo "Pushing the app in docker Hub."
         withCredentials([usernamePassword('credentialsId':"Docker Hub Cred", passwordVariable:"dockerHubPass", usernameVariable:"dockerhubUser")])
         {
             sh "docker login -u ${env.dockerhubUser} -p ${env.dockerHubPass}"
-            sh "docker image tag notes-app:latest ${env.dockerhubUser}/notes-app:latest"
-            sh "docker push ${env.dockerhubUser}/notes-app:latest"
+            sh "docker image tag "${projectname}":"${imagename}" ${env.dockerhubUser}/"${projectname}":"${imagename}""
+            sh "docker push ${env.dockerhubUser}/"${projectname}":"${imagename}""
         }
 }
