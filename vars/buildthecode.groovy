708dbaae170bf6142dd191bc48f7b24da271d629
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-12-16 13:06:24 +0545

    Modify call function to accept parameters for docker build

--- vars/buildthecode.groovy
+++ vars/buildthecode.groovy
@@ -1,4 +1,3 @@
-def call() {
-      echo "Building the app in docker."
-      sh "docker build -t notes-app:latest ."
+def call(String projectname, String imagename, String dockerhubuser) {
+      sh "docker build -t "${dockerhubuser}"/"${projectname}":"${imagename}" ."
 }
